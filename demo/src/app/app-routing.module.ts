4c93deb1b48d8aceed20d4d74d3f36e31f713c34
Harsh Darji <darjiharsh52@gmail.com>
2021-02-22 17:33:49 +0530

    get data

--- demo/src/app/app-routing.module.ts
+++ demo/src/app/app-routing.module.ts
@@ -7,13 +7,16 @@ import { NotFoundComponent } from './module/not-found/not-found.component';
 import { AuthguardGuard } from './service/authguard.guard';
 import { LoginComponent } from './authguard/login/login.component';
 import { DashboardComponent } from './authguard/dashboard/dashboard.component';
-
+import { ResReqComponent } from './res-req/res-req.component';
+import { ReqResLoginComponent } from './req-res-login/req-res-login.component';
 const routes: Routes = [
  
   {path: 'login', component: LoginComponent},
+  {path:'login1', component:ReqResLoginComponent},
     {path: 'dashboard', component: DashboardComponent, canActivate: [AuthguardGuard] },
     
- 
+    {path: 'list', component: ResReqComponent}
+, 
   { 
     path: 'employee', 
     component: EmployeeComponent,
@@ -29,6 +32,10 @@ const routes: Routes = [
     component: CompanyComponent,
     loadChildren: () => import('./module/company/company.module').then(m => m.CompanyModule),
 },
+
+
+
+
 { 
     path: '**', 
     component: NotFoundComponent 
@@ -44,8 +51,11 @@ export const routingComponents = [ EmployeeComponent,
   CmsComponent,
   CompanyComponent,
   NotFoundComponent,
-  LoginComponent,
+  // LoginComponent,
   DashboardComponent,
+  ResReqComponent,
+  ReqResLoginComponent
+  
  
 
 ]
--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -7,22 +7,25 @@ import { AppComponent } from './app.component';
 import { ErrorHandler} from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthguardGuard } from './service/authguard.guard';
-
-
+import { ResReqComponent } from './res-req/res-req.component';
+import { ReqResLoginComponent } from './req-res-login/req-res-login.component';
+import {MatCardModule} from '@angular/material/card';
+import { globalErrorHandler } from './error/global-errorHandler';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-   
-   
-
-   
+    ResReqComponent,
+    ReqResLoginComponent,   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule 
+    HttpClientModule,
+    BrowserAnimationsModule ,
+    MatCardModule
   ],
   providers: [AuthguardGuard,
     
