13b461bc40449255d74e93fd6b4465cf14a04a4d
Harsh Darji <darjiharsh52@gmail.com>
2021-03-21 17:19:27 +0530

    Notification

--- angular-pwa/src/app/app.module.ts
+++ angular-pwa/src/app/app.module.ts
@@ -6,29 +6,27 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { HttpClientModule } from '@angular/common/http';
-import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
+
+
 import { HomeComponent } from './home/home.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { AuthService } from './core/services/auth.service';
+
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireMessagingModule } from '@angular/fire/messaging';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { EmployeeService } from './core/services/employee.service';
-import { AssComponent } from './ass/ass.component';
+
 // import { MessagingService } from './core/services/notification/messaging.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegistrationComponent,
     HomeComponent,
-    DashboardComponent,
-    AssComponent
+
+  
   ],
   imports: [
     BrowserModule,
@@ -45,7 +43,7 @@ import { AssComponent } from './ass/ass.component';
   ],
   providers: [
     EmployeeService,
-    AuthService,
+   
   ],
   bootstrap: [AppComponent]
 })
